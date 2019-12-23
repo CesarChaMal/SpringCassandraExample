6f394aa3b0e5bf8becd150640d4e449485a30325
Cesar Francisco Chavez Maldonado <cesarchamal@gmail.com>
2019-12-23 23:10:37 +0100

    Upload code for SpringCassandraExample project.

---
+++ src/com/jcg/examples/repo/PersonRepo.java
@@ -0,0 +1,10 @@
+package com.jcg.examples.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jcg.examples.entity.Person;
+
+public interface PersonRepo extends CrudRepository<Person, String>
+{
+
+}
